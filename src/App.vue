3a4ca5c72ad7d26239850e643a58d9825dd339e2
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-06-02 10:41:09 -0300

    Segundo commit vue-calculadorda

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalculadoraComponet msg="Calculadora"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculadoraComponet from './components/CalculadoraComponet.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CalculadoraComponet
   }
 }
 </script>
---
+++ src/components/CalculadoraComponet.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table class="table table-bordered table-dark">
+      <tbody>
+        <tr>
+          <td colspan="4" class="resultado">{{ output || 0 }}</td>
+        </tr>
+        <tr class="sobrecor">
+          <td v-on:click="addNumber(7)">7</td>
+          <td v-on:click="addNumber(8)">8</td>
+          <td v-on:click="addNumber(9)">9</td>
+          <td v-on:click="addOperate('/')">/</td>
+        </tr>
+        <tr class="sobrecor">
+          <td v-on:click="addNumber(4)">4</td>
+          <td v-on:click="addNumber(5)">5</td>
+          <td v-on:click="addNumber(6)">6</td>
+          <td v-on:click="addOperate('*')">*</td>
+        </tr>
+        <tr class="sobrecor">
+          <td v-on:click="addNumber(1)">1</td>
+          <td v-on:click="addNumber(2)">2</td>
+          <td v-on:click="addNumber(3)">3</td>
+          <td v-on:click="addOperate('-')">-</td>
+        </tr>
+        <tr class="sobrecor">
+          <td v-on:click="addPonto('.')">.</td>
+          <td v-on:click="addNumber(0)">0</td>
+          <td v-on:click="addOperate('%')">%</td>
+          <td v-on:click="addOperate('+')">+</td>
+        </tr>
+        <tr class="sobrecor">
+          <td v-on:click="clearField" class="clear">C</td>
+          <td v-on:click="addOperate('+/-')">+/-</td>
+          <td colspan="1" class="del" v-on:click="deleteUltimoCaractere()">X</td>
+          <td class="enter" v-on:click="calculate()">=</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculadora',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      output: '0',
+      previousValue: null,
+      operator: null,
+      accounts: []
+    };
+  },
+  methods: {
+    clearField() {
+      this.output = '0';
+    },
+    addNumber(number) {
+      if (this.output === '0') {
+        this.output = number.toString();
+      } else {
+        this.output += number.toString();
+      }
+    },
+    addOperate(operator) {
+      if (this.output === '0') {
+        this.output = operator.toString();
+      } else {
+        this.output += operator.toString();
+      }
+    },
+    addPonto(value) {
+      if (this.output === '0' && value !== '.') {
+        this.output = value;
+      } else {
+        this.output += value;
+      }
+    },
+    calculate() {
+      let expression = this.output;
+
+      if (expression.includes('+')) {
+        const numbers = expression.split('+');
+        const sum = numbers.reduce((acc, curr) => Number(acc) + Number(curr));
+        this.output = sum.toString();
+      } else if (expression.includes('-')) {
+        const numbers = expression.split('-');
+        const difference = numbers.reduce((acc, curr) => Number(acc) - Number(curr));
+        this.output = difference.toString();
+      } else if (expression.includes('*')) {
+        const numbers = expression.split('*');
+        const product = numbers.reduce((acc, curr) => Number(acc) * Number(curr));
+        this.output = product.toString();
+      } else if (expression.includes('/')) {
+        const numbers = expression.split('/');
+        const quotient = numbers.reduce((acc, curr) => Number(acc) / Number(curr));
+        this.output = quotient.toString();
+      } else if (expression.includes('%')) {
+        this.output = (parseFloat(this.output) / 100).toString();
+      } else if (expression.includes('+/-')) {
+        this.output = (parseFloat(this.output) * -1).toString();
+      }
+      this.accounts.push(expression);
+    },
+    deleteUltimoCaractere() {
+      this.output = this.output.substr(0, this.output.length - 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+td {
+  user-select: none;
+}
+.enter{
+  background-color: #1fc448;
+}
+.del{
+  background-color: rgb(240, 18, 18);
+}
+.sobrecor{
+  font-size: 40px;
+}
+.resultado{
+  font-size: 40px;
+}
+.sobrecor :hover{
+  background-color: rgb(163, 159, 159);
+}
+.clear{
+  background-color: rgb(250, 175, 37);
+}
+.sobrecor :active{
+  background-color: black;
+}
+h1{
+  font-size: 60px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
